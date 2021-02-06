3dc3083370b9b860c32c7931cae26d509ccfeb7f
Josh Wilson <joshua.wilson6289@gmail.com>
2021-02-06 12:29:50 -0600

    succesfully mapping over a players array

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,67 @@
 import React, { useState, useEffect } from "react";
 import PlayerRow from "./PlayerRow";
 
-const Table = () => {
+// generic player array
+const players = [
+  {
+    name: "Lemar O",
+    team: "Grey",
+    goals: 2,
+    assists: 5,
+    points: 7,
+    pim: 0,
+    ppg: 2.33
+  },
+  {
+    name: "Adam J",
+    team: "Grey",
+    goals: 0,
+    assists: 1,
+    points: 1,
+    pim: 0,
+    ppg: 0.33
+  },
+  { 
+    name: "Mike B",
+    team: "Orange",
+    goals: 4,
+    assists: 1,
+    points: 5,
+    pim: 0,
+    ppg: 1.67
+  }
+]
 
-  // take in tabletop as an array of objects (each row) and set it as state here
+const Table = () => {
 
   const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
-    
-  // get data here
+
+    setTableData(players);
     
   },[tableData]);
+
+  let renderedPlayers = players.map((player) => {
+    return <PlayerRow
+      key={player.name}
+      name={player.name}
+      team={player.team}
+      goals={player.goals}
+      assists={player.assists}
+      points={player.points}
+      pim={player.pim}
+      ppg={player.ppg}
+    />
+  });
+  
 
   return (
     <table className="ui celled table">
       <thead>
         <tr>
           <th>Player
-     
+
           </th>
           <th>Team
 
@@ -42,7 +85,7 @@ const Table = () => {
       </thead>
       
       <tbody>
-        <PlayerRow />
+        {renderedPlayers}
       </tbody>
 
     </table>
