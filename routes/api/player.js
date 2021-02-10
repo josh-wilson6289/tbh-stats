2f323acd17b23f4ff762938be0985de12bb17c86
Josh Wilson <joshua.wilson6289@gmail.com>
2021-02-10 12:27:40 -0600

    create route for player added

--- routes/api/player.js
+++ routes/api/player.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const playerController = require("../../controllers/playerController");
 
 router.route("/")
-  .get(playerController.findAll);
+  .get(playerController.findAll)
+  .post(playerController.create);
   
 module.exports = router;
