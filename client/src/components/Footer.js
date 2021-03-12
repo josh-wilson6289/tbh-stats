299e24dcc53f940a239d13f6096b455ef5630eee
Josh Wilson <joshua.wilson6289@gmail.com>
2021-03-11 18:31:57 -0600

    styling footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,12 +3,47 @@ import "../style.css";
 
 
 const Footer = () => {
+  
   return (
-    <div className="footer">
-    <div className="footer-bg">
-      Texas Ball Hockey 
-    </div>
-    </div>
+<div className="footer container-fluid">
+ <div className="row justify-content-center">
+      <div className="col-md-12">
+        <a href="/" className="logo-footer">
+          <img className="responsive" src="http://texasballhockey.com/wp-content/themes/tbh-new-flex/img/logo.png" alt="Texas Ball Hockey"></img>
+        </a>
+        <div className="location">
+                <p className="location">
+                  3918 Gattis School Rd #102
+                  Round Rock, TX 78664
+                </p>
+        </div>
+          
+          <div className="footer-social">
+              <ul>
+                <li>
+                  <a href="https://www.facebook.com/texasballhockey/"><i className="fab fa-facebook"></i></a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/texasballhockey"><i className="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/texasballhockey"><i className="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/channel/UCQEtMyZzpMMtQZSZFxmf04A"><i className="fab fa-youtube"></i></a>
+                </li>
+              </ul>
+          </div>
+      </div>
+  </div>
+</div>
+
+            
+   
+
+
+
+
   );
 };
 
