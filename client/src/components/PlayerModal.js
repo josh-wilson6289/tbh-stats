522ab9b4a07805f24ff17340428070654c186e91
Josh Wilson <joshua.wilson6289@gmail.com>
2021-03-31 11:34:34 -0500

    placeholder photo in card

--- client/src/components/PlayerModal.js
+++ client/src/components/PlayerModal.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import "../style.css";
+import PersonPlaceholder from "../Images/PersonPlaceholder.png";
 
 const PlayerModal = (props) => {   
   return (
@@ -10,17 +12,28 @@ const PlayerModal = (props) => {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          {`${props.player.firstName} ${props.player.lastName}`}
-        </Modal.Title>
+   
+      <Modal.Header className="float-right" closeButton>
+      <h1 className="modal-title">{`${props.player.firstName} ${props.player.lastName}`}</h1>
       </Modal.Header>
-      <Modal.Body>
+      <div className="player-card-modal">
+      <Modal.Body>      
+       <div className="container-fluid">
+         <div className="row">
+           <div className="col-md-4">
+             <img src={PersonPlaceholder} alt="placeholder"></img>
+           </div>
+         </div>
+       </div>
 
       </Modal.Body>
+      </div>
       <Modal.Footer>
+        <div className="modal-btn">
         <Button onClick={props.onHide}>Close</Button>
+        </div>
       </Modal.Footer>
+    
     </Modal>
   );
 }
